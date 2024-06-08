b09d84890b683b385e8cf04988c2f2abc186734c
shashi kant <shashikant@pbpartners.com>
2024-06-09 01:47:07 +0530

    add paid pdf f

--- src/Components/AddPaidFiles/AddPaidFiles.jsx
+++ src/Components/AddPaidFiles/AddPaidFiles.jsx
@@ -5,9 +5,12 @@ function AddPaidFiles() {
  
     const [data ,setData] = useState({
          cat:{name:"cat" ,value:"", error:"" , options:[], message:""},
-         file:{name:"file" ,value:"", error:"" , message:""}
-    }) 
-     
+         file:{name:"file" ,value:"", error:"" , message:"" ,extension:"" , } 
+                
+     }) 
+
+     const [ loader , setLoader ]  = useState(false)
+    
 
     const {cat , file }  = data 
 
@@ -24,12 +27,25 @@ function AddPaidFiles() {
 
 
     const onChangeHandler= (e)=>{
-         const { name  }  = e.target
+        debugger 
+        const { name  }  = e.target
          
+         
+        
          if(name=="file") {
+
+            let filename = e.target.value
+            let file_arr = filename.split('.')
+            let ext = (file_arr[file_arr.length-1]).toLowerCase() 
+             
+             if(ext!= "pdf" ) {
+                alert("please insert pdf file only")
+                return
+             }
+
              setData(p=>{
                 return{
-                    ...p , [name]:{...p[name], value:e.target.files[0]}
+                    ...p , [name]:{...p[name], value:e.target.files[0] , extension:ext}
                 }
             })
              
@@ -42,16 +58,46 @@ function AddPaidFiles() {
             })
          }
     }
+    
 
-    const insertPaidFile =async ()=>{
-          let formData = new FormData()
+
+     const insertPaidFile =async ()=>{
+           
+        setLoader(true)
+        
+        for(let key in data) {
+           if(!data[key]['value']) {
+              alert("please fill all the fields")
+              return
+           }    
+
+           if(!data[key]['value'] && key=="file") {
+              
+               if(data[key]['extension']!=='pdf')
+                {
+                    alert("please select pdf only , then retry ") 
+                    return 
+                }
+               
+           }
+            
+
+        }
+
+
+        alert("pdf saved")
+        let formData = new FormData()
           formData.append('attachment' , data.file.value)
           formData.append('catId' , data.cat.value)
+    
            let saved = await savePaidPdf('insert-paid-files' , formData )
-            console.log("saved",saved)
             if(saved.status) {
+                setLoader(false)
                   alert('saved')
             }
+
+            setLoader(false)
+
         } 
 
 
@@ -62,30 +108,29 @@ function AddPaidFiles() {
          <div className='row'>
               <div className='col-12'>
                 <div class="form-group" >
-                    <label for="exampleInputEmail1">Category</label>
 
  
                         <div class="form-group">
-                            <label for="exampleFormControlSelect1">Example select</label>
                             <select type="text" onChange={onChangeHandler} name={cat.name} value={cat.value} class="form-control" id="exampleFormControlSelect1">
-                            
+                             <option value=""> please select category first  </option>
                             {cat.options.map(ele=>{
                                  return  <option value={ele._id}>{ele.TITLE}</option>
                             })}
 
                             </select>
                         </div>
-                        
-                                        
-                  
+
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Files</label>
                     <input type="file" name={file.name}  onChange={onChangeHandler} class="form-control"   />
                 </div>
-          
-                <button  class="btn btn-primary" onClick={insertPaidFile} >Insert</button>
                  
+               {loader ?   <button disabled class="btn btn-primary" > loader </button> :
+               <button  class="btn btn-primary" onClick={insertPaidFile} >Insert</button>
+                 }
+            
+              
               </div>
         </div>
          
